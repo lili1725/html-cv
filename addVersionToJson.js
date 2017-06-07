b161206d710c8e0a96dc2cb825211b3d746b2a64
lili1725 <alice.rimassa@gmail.com>
2017-06-07 23:10:22 +0200

    Started to work on  cache buster : todo put it to images

--- addVersionToJson.js
+++ addVersionToJson.js
@@ -1,3 +1,20 @@
-/**
- * Created by USER on 05/06/2017.
- */
+(function() {
+
+    let indexJson = require('./fr-trad.json');
+
+    if(!indexJson) {
+        console.error('Missing translation json file');
+        indexJson = {};
+    }
+
+    require('child_process').exec('git log --pretty=format:\'%h\' -n 1', function(err, stdout) {
+        console.log('Last commit hash on this branch is:', stdout);
+        indexJson.version = stdout;
+        require('fs').writeFile('./index.json', JSON.stringify(indexJson), function(err) {
+                if (err) throw err;
+                console.log('complete');
+            }
+        );
+    });
+})();
+
